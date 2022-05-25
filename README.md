f2030cf4b96dcd52910d10155982a2ef61d21121
Dmitry <dragoniiix@mail.ru>
2022-05-25 14:40:44 +0300

    project creation

---
+++ README.md
@@ -0,0 +1,5 @@
+## Practice based on real technical tasks 
+
+In this project I consolidate the practical skills of JavaScript, before learning how to work, I also plan to form a pet-project: ["Quiz questions"](#) 
+
+>Link coming soon =)
---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+import './slider';
+import modals from './modules/modals';
+
+// I am well aware that there are warnings, but so far I do not plan to fix them, I just practice and do not want to get into the work boostrap, then I plan to work without it, and waste time on this now can not
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  modals();
+
+  // const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
+  //   modalEngineer = document.querySelector('.popup_engineer'),
+  //   modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
+  // bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+   
+});
