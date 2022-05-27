8150d54e155018f278f11bacdc25b8fb0895a7ed
Dmitry <dragoniiix@mail.ru>
2022-05-27 16:59:15 +0300

    added function zoom for images

--- dist/script.js
+++ dist/script.js
@@ -14015,6 +14015,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _modules_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/forms */ "./src/js/modules/forms.js");
 /* harmony import */ var _modules_changeModalState__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/changeModalState */ "./src/js/modules/changeModalState.js");
 /* harmony import */ var _modules_timer__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/timer */ "./src/js/modules/timer.js");
+/* harmony import */ var _modules_images__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./modules/images */ "./src/js/modules/images.js");
+
 
 
 
@@ -14039,6 +14041,7 @@ window.addEventListener('DOMContentLoaded', () => {
   Object(_modules_tabs__WEBPACK_IMPORTED_MODULE_2__["default"])('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
   Object(_modules_forms__WEBPACK_IMPORTED_MODULE_3__["default"])(modaleState);
   Object(_modules_timer__WEBPACK_IMPORTED_MODULE_5__["default"])('.container1', deadline);
+  Object(_modules_images__WEBPACK_IMPORTED_MODULE_6__["default"])();
 });
 
 /***/ }),
@@ -14200,6 +14203,53 @@ const forms = state => {
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (forms);
+
+/***/ }),
+
+/***/ "./src/js/modules/images.js":
+/*!**********************************!*\
+  !*** ./src/js/modules/images.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const images = () => {
+  const workContainer = document.querySelector('.works'),
+        popupImg = document.createElement('div'),
+        bigImg = document.createElement('img');
+  popupImg.classList.add('popup');
+  workContainer.append(popupImg);
+  popupImg.style.cssText = `
+    display: none;
+    justify-content: center;
+    align-items: center;
+  `;
+  popupImg.append(bigImg);
+  bigImg.style.cssText = `
+    max-wight: 900px;
+    height: 60vh;
+  `;
+  workContainer.addEventListener('click', e => {
+    e.preventDefault();
+    const target = e.target;
+
+    if (target && target.classList.contains('preview')) {
+      popupImg.style.display = 'flex';
+      const path = target.parentNode.getAttribute('href');
+      bigImg.setAttribute('src', path);
+      document.body.style.overflow = 'hidden';
+    }
+
+    if (target && target.matches('div.popup')) {
+      popupImg.style.display = 'none';
+      document.body.style.overflow = '';
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (images);
 
 /***/ }),
 
--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ import tabs from './modules/tabs';
 import forms from './modules/forms';
 import changeModalState from './modules/changeModalState'; 
 import timer from './modules/timer';
+import images from './modules/images';
 
 /*
 I know about the warnings associated with boostrap, but I will not eliminate them at this time, 
@@ -24,5 +25,5 @@ window.addEventListener('DOMContentLoaded', () => {
   tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img','do_image_more', 'inline-block');
   forms(modaleState);
   timer('.container1', deadline);
-
+  images();
 });
