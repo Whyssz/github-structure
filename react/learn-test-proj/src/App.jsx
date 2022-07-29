ecec9b8ecfd211723a244158094bfa18c939997e
Shilov Dmitry <dragoniiix@mail.ru>
2022-07-29 18:53:37 +0300

    add useing lib bootstrap

--- react/learn-test-proj/src/App.jsx
+++ react/learn-test-proj/src/App.jsx
@@ -1,5 +1,27 @@
 import { Component } from 'react';
+
+import styled from 'styled-components';
 import './App.css';
+
+const EmpItem = styled.div`
+  padding: 20px;
+  margin-bottom: 30px;
+  border-radius: 5px;
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+`
+
+const Header = styled.h2`
+  font-size: 22px;
+`
+
+const Button = styled.button`
+  display: block;
+  padding: 5px 15px;
+  background-color: gold;
+  border: 1px solid rgba(0,0,0, .2);
+  box-shadow: 5px 5px 10px rgba(0,0,0, .2);
+  cursor: pointer;
+`
 
 class WhoAmI extends Component {
   constructor(props) {
@@ -27,30 +49,35 @@ class WhoAmI extends Component {
   render() {
     const {name, surname, links} = this.props
     const {years, position, text} = this.state
-
+    
     return (
-      <>
-        <h2>My name is {name}, 
+      <EmpItem>
+        <Header>My name is {name}, сд
           surname - {surname}, 
           age - {years}, 
-          position - {position}</h2>
+          position - {position}</Header>
         <a href={links}>Hi</a>
         <form>
           <span>Введите должность</span>
           <input type="text" onChange={(e) => this.commitInputChange(e, 'dark')} />
         </form>
-        <button onClick={this.nextYear}>{text}</button>
-      </>
+        <Button onClick={this.nextYear}>{text}</Button>
+      </EmpItem>
     )
   }
 }
 
+const Wrapper = styled.div`
+  width: 600px;
+  margin: 80px auto;
+`
+
 function App() {
   return (
-    <div className='App'>
+    <Wrapper>
       <WhoAmI name='Ivan' surname='Petrichenko' links='facebook.com'/>
       <WhoAmI name='Dima' surname='Shilov' links='444'/>
-    </div>
+    </Wrapper>
   )
 }
 
