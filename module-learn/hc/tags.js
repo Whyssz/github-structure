12ff410c9156edd5487300576a515a6de36d1d77
Shilov Dmitry <dragoniiix@mail.ru>
2022-08-01 11:10:38 +0300

    add info about html/css semantice

---
+++ module-learn/hc/tags.js
@@ -0,0 +1,60 @@
+/*
+Main body____
+  <header>
+  <main>
+  <foorter>
+  <section>
+  
+Main tags____
+  
+  <article> - смысловой блок (пост, твит, комметнт, единичный блок - ad)
+  <aside> - косвенный блок/побочная информация (не боковой блок)
+  <nav> - меню
+  <div> - потоковые блоки
+  <span> - единичные слова / выделения
+
+  <p> - text 
+  <i> - под иконку
+  <blockquote> - цитата
+  
+Group tags___
+  <ul/li-ol/> - list
+  <table>{
+    tr{
+      th - column name
+    }
+    tr{
+      th - row name
+      td - row value
+    }
+  } 
+  <dl>{
+    dt - title
+    dd - text
+  }
+  <form>{
+    action name methods
+    enctype - type application/multipart/text
+  }
+  <select>{
+    name 
+    size - count rows
+    multiply - some choices
+  }
+  <input>{
+    type - button / checkbox / date / datetime / datetime-local / email / file /image / number / password / radio / range / reset / search /submit / tel / text / time /
+    accept - type save files
+    cheacked 
+    name 
+    value
+  }
+  <label> {
+    for='input'
+
+  }
+  <button>{
+    name value
+    type - submit reset button
+  }  
+
+*/
