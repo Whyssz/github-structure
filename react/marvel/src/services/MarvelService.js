4b3afbf4978a6626e36572e6fe31f45aa99f8656
Shilov Dmitry <dragoniiix@mail.ru>
2022-08-03 12:53:00 +0300

    func of getting characters addded and the func of adding a new character was corrected

--- react/marvel/src/services/MarvelService.js
+++ react/marvel/src/services/MarvelService.js
@@ -1,1 +1,42 @@
+class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=6580aaf3f7ebb7dd34e71091f73f8943';
 
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could no fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  getAllCharacter = async () => {
+    const res = await this.getResource(
+      `${this._apiBase}characters?limit=9&offset=220&${this._apiKey}`
+    );
+    return res.data.results.map(this._transformCharactre)
+  };
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(
+      `${this._apiBase}characters/${id}?${this._apiKey}`
+    );
+    return this._transformCharactre(res.data.results[0]);
+  };
+
+  _transformCharactre = (char) => {
+    return {
+      name: char.name,
+      description: char.description
+        ? `${char.description.slice(0, 198)}...`
+        : "The charecter doesn't have a description",
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      homepage: char.urls[0].url,
+      wiki: char.urls[1].url,
+    };
+  };
+}
+
+export default MarvelService;
