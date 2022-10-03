8344c607b672238dabcf660d3864990789ad7cd7
Shilov Dmitry <dragoniiix@mail.ru>
2022-10-03 18:10:47 +0300

    add info about ts

--- learn/ts/ts.js
+++ learn/ts/ts.js
@@ -1,8 +1,60 @@
 /*
+  TYPE: 
+    1. string
+    2. number 
+    3. boolean
+    4. null // obj - отсутствие объектного значения
+    5. undefained 
+    6. object
+    7*. symbol
+    8*. bigint
+
+	void - инчего не возвращает
+	never - возвразает ошибку или постоянно выполняется
+
+	enum 
+	type
+
+	classic structure in an typescript
+
+	(function(Directions){
+		...
+	})(Direction || (Directions = {}));
+*/
+
+/*
   import PropTypes from 'prop-types'
+
   Name.propTypes = {
     function: PropTypes.func.isRequired
   }
+*/
 
+/*
+	EX: 
+		const createProf = (name: string, ...skills:Array<any>):string => `${name}, skills:${skills}`
+
+		type Person = {
+				name: string, 
+				age: number | string,
+				nickName?: string,
+				getPass?: () => string
+		}
+
+		const user: Person = {
+				name: 'Dima', 
+				age: 24,
+				nickName: 'webDev'
+		}
+
+		const user2: Person = {
+				name: 'Alex',
+				age: 23,
+				getPass():string {
+						return `${this.name} - ${this.age}` 
+				}
+		}
+
+	EX: class 
 
 */
