4c08cd48b5659dda66c99adb2719314c79c0e2d2
Shilov Dmitry <dragoniiix@mail.ru>
2023-01-24 16:00:52 +0300

    upd: refactoring/restructuring

--- react/tailwind/README.md
+++
@@ -1,1 +0,0 @@
-Basic tailwind practice
--- react/ts/ts-app/src/store/actionCreators/index.ts
+++
@@ -1,7 +0,0 @@
-import * as TodoActionCreators from '../actionCreators/todo';
-import * as UserActionCreators from '../actionCreators/user';
-
-export const AllActions = {
-  ...UserActionCreators,
-  ...TodoActionCreators
-};
