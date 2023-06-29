f39eda6ca558431be4ac304ce15ba0a232fef37a
Shilov Dmitry <dragoniiix@mail.ru>
2023-06-29 19:15:13 +0300

    add stylelint and i18next

--- projects/libs/client/src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ projects/libs/client/src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -4,7 +4,6 @@ import DarkIcon from 'shared/assets/icons/theme-dark.svg';
 import LightIcon from 'shared/assets/icons/theme-light.svg';
 import { classNames } from 'shared/lib/classNames';
 import { Button, ButtonStyle } from '../Button/Button';
-import styles from './ThemeSwitcher.module.scss';
 
 interface ThemeSwitcherProps {
 	className?: string;
@@ -14,12 +13,14 @@ export const ThemeSwitcher: FC<ThemeSwitcherProps> = ({ className }) => {
 	const { theme, toggleTheme } = useTheme();
 
 	return (
-		<Button
-			theme={ButtonStyle.CLEAR}
-			className={classNames(styles.themeSwitcher, {}, [className])}
-			onClick={toggleTheme}
-		>
-			{theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
-		</Button>
+		<>
+			<Button
+				theme={ButtonStyle.CLEAR}
+				className={classNames('', {}, [className])}
+				onClick={toggleTheme}
+			>
+				{theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+			</Button>
+		</>
 	);
 };
--- projects/libs/client/src/widgets/Navbar/ui/Navbar.tsx
+++ projects/libs/client/src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import { type FC } from 'react';
+import { useTranslation } from 'react-i18next';
 import { AppLinkTheme } from 'shared/config/themeConfig/theme.config';
 import { classNames } from 'shared/lib/classNames';
 import { AppLink } from 'shared/ui/AppLink/AppLink';
@@ -9,14 +10,16 @@ interface NavbarProps {
 }
 
 export const Navbar: FC<NavbarProps> = ({ className }) => {
+	const { t } = useTranslation();
+
 	return (
 		<div className={classNames(styles.navbar, {}, [className])}>
 			<div className={styles.links}>
 				<AppLink theme={AppLinkTheme.PRIMARY} to='/'>
-					Home
+					{t('Страница главная')}
 				</AppLink>
 				<AppLink theme={AppLinkTheme.INVERTED} to='/about'>
-					About
+					{t('Страница о нас')}
 				</AppLink>
 			</div>
 		</div>
